fa48f0aebe29ac3eedffa77d66d47d56456dac1e
MacosPrintes001 <67763353+MacosPrintes001@users.noreply.github.com>
2021-09-06 10:05:06 -0300

    botMain feito

--- botMain.py
+++ botMain.py
@@ -9,22 +9,37 @@ bot = telebot.TeleBot(token=token)
 def start(message):
     id_telegram = message.from_user.id
     chat_id = message.chat.id
-    nome = f"{message.from_user.first_name} {message.from_user.last_name}"
+    nome = f"{message.from_user.first_name}"
 
-    #A FAZER: verificar se o discente está registrado no banco
-
-    if True: #se o _discente não vazio  
-        bot.send_message(chat_id, f"Olá {nome} sejá bem vindo ao sistema virtual da UFOPA. No menu abaixo selecione o NÚMERO da sua opção")
-        menu(message)
-        
-    else:
-        #realizar cadastro do cara
-        pass
-
+    bot.send_message(chat_id, f"Olá {nome} sejá bem vindo ao sistema virtual da UFOPA. Para acessar as proximas etapas você precisa estar cadastrado(a) no sistema \
+                                me diga, você já esta cadastrado(a)?")
+    r = bot.send_message(chat_id, "1 - Sim\n2 - Não")
+    bot.register_next_step_handler(r, registrado)
 #função registrar usuario
 
+
+def registrado(message):
+    try:
+        chat_id = message.chat.id
+        resp = int(message.text)
+        link = "http://www.minhavidaacademica.com.br/View/discente/cadastrar_disc.php"
+        if resp == 1:
+            menu(message)
+        elif resp == 2:
+            bot.send_message(chat_id, f"Certo, entre no link abaixo, faça seu cadastro e depois volte aqui")
+            bot.send_message(chat_id, f"{link}")
+        else:
+            bot.send_message(chat_id, "Desculpe não entedi o que disse. Você já possui cadastro?")
+            r = bot.send_message(chat_id, "1 - Sim\n2 - Não")
+            bot.register_next_step_handler(r, registrado)
+    except Exception:
+        bot.send_message(message.chat.id, "Responda com NÚMERO da sua opção")
+        r = bot.send_message(chat_id, "1 - Sim\n2 - Não")
+        bot.register_next_step_handler(r, registrado)
+            
 def menu(message):
     chat_id = message.chat.id
+    bot.send_message(chat_id, "OK, o que eu posso fazer por você?")
     m = bot.send_message(chat_id, "1 - Agendamento\n2 - Requisição de Atestado de Matrícula\n3 - Abono de Faltas")
     bot.register_next_step_handler(m, encaminhaBot)
 
@@ -35,8 +50,8 @@ def encaminhaBot(message):
     if opc == 1:
         bot.send_message(chat_id, "OK, basta você falar com meu amigo @ufopa_agendamento para realiazar seu agendamento")
     else:
-        m = bot.send_message(chat_id, "Esta função ainda não foi implementada, escolha outra")
-        bot.register_next_step_handler(m, menu)
+        bot.send_message(chat_id, "Esta função ainda não foi implementada, escolha outra")
+        menu(message)
 
 
 @bot.message_handler(func=lambda m : True )
