427f55d07bb579baa473777467a0b27f8a11b39c
MacosPrintes001 <67763353+MacosPrintes001@users.noreply.github.com>
2022-04-12 13:08:33 -0300

    função stop implementada

--- botAgendamento.py
+++ botAgendamento.py
@@ -1,7 +1,5 @@
-from telebot.util import per_thread
 import dados_bot
 import telebot
-from random import randint
 import conection as cnt
 import botUtil
 
@@ -23,11 +21,8 @@ def start(message):
 
 @bot.message_handler(commands=['stop'])
 def stop(message):
-    print("Entrei stop")
     bot.reply_to(message, "Certo, quando quiser realizar uma solicitação basta me chamar que estarei aqui")
-    print("Mandei uma")
     bot.send_message(message.chat.id, "Serviço encerrado")
-    print("Mandei duas")
 
 
 
@@ -146,31 +141,37 @@ def requestHour(message):
     try:
         chat_id = message.chat.id
         user = str(message.from_user.id)
-        recurso_Usuario = recursos[int(message.text)] #EXTRAI DE RECURSOS OS DADOS DA OPÇÃO DO USUARIO
-        resp, errorCode, menu, id_recurso = cnt.verific_id(recurso_Usuario) #VERIFICA SE A OPÇÃO ESCOLHIDA É VALIDA
+        msg = str(message.text).lower().strip()
 
-        if resp: 
-            botUtil.writeText(user, "recurso_campus_id_recurso_campus", id_recurso)
-
-            menuHour, hora = cnt.getHora(id_recurso)
-
-            # ATRIBUI À VARIAVEL GLOBAL O DICIONARIO COM AS HORAS
-            horarios = hora
-
-            r = bot.send_message(chat_id, f"Escolha a opção referente aos horarios abaixo.\n{menuHour}")
-            bot.register_next_step_handler(r, procHour)
+        if "/stop" in msg:
+            return stop(message)
 
         else:
-            if errorCode == 4:  # id não encontrado
-                bot.send_message(chat_id, "Opção não reconhecida, tente novamente")
-                r = bot.send_message(chat_id, f"{menu}")
-                bot.register_next_step_handler(r, requestHour)
-            elif errorCode == 5:  # algo que não é um id
-                m = bot.send_message(chat_id, f"Houve um erro no servidor, tente novamente.\n{menu}")
-                bot.register_next_step_handler(m, requestHour)
+            recurso_Usuario = recursos[int(msg)] #EXTRAI DE RECURSOS OS DADOS DA OPÇÃO DO USUARIO
+            resp, errorCode, menu, id_recurso = cnt.verific_id(recurso_Usuario) #VERIFICA SE A OPÇÃO ESCOLHIDA É VALIDA
+
+            if resp: 
+                botUtil.writeText(user, "recurso_campus_id_recurso_campus", id_recurso)
+
+                menuHour, hora = cnt.getHora(id_recurso)
+
+                # ATRIBUI À VARIAVEL GLOBAL O DICIONARIO COM AS HORAS
+                horarios = hora
+
+                r = bot.send_message(chat_id, f"Escolha a opção referente aos horarios abaixo.\n{menuHour}")
+                bot.register_next_step_handler(r, procHour)
+
             else:
-                m = bot.send_message(chat_id, f"Houve um erro, tente novamente.\n{menu}")
-                bot.register_next_step_handler(m, requestHour)
+                if errorCode == 4:  # id não encontrado
+                    bot.send_message(chat_id, "Opção não reconhecida, tente novamente")
+                    r = bot.send_message(chat_id, f"{menu}")
+                    bot.register_next_step_handler(r, requestHour)
+                elif errorCode == 5:  # algo que não é um id
+                    m = bot.send_message(chat_id, f"Houve um erro no servidor, tente novamente.\n{menu}")
+                    bot.register_next_step_handler(m, requestHour)
+                else:
+                    m = bot.send_message(chat_id, f"Houve um erro, tente novamente.\n{menu}")
+                    bot.register_next_step_handler(m, requestHour)
     
     except Exception:
         m = bot.send_message(chat_id, f"Houve um erro no servidor, tente novamente.\n{menu}")
@@ -179,22 +180,25 @@ def requestHour(message):
 
 def procHour(message):
     # RETORNA AS HORAS REFERENTE A OPÇÃO ESCOLHIDA
-    
     try:
-        numb = int(message.text)
+        msg = str(message.text).strip().lower()
         chat_id = message.chat.id
         user = str(message.from_user.id)
 
-        resp = botUtil.isTime(horarios, numb, user)
+        if "/stop" in msg:
+            return stop(message)
 
-        if resp:
-            bot.send_message(chat_id, "Tudo certo, Hora valida. Mande a data para a qual vai ser a reserva.")
-            r = bot.send_message(chat_id, "Mande a data no seguinte formato dd/mm/yyyy")
-            bot.register_next_step_handler(r, procDate)
-        
-        else:
-            r = bot.send_message(chat_id, "Horario inválido. Tente novamente")
-            bot.register_next_step_handler(r, procHour)
+        else:    
+            resp = botUtil.isTime(horarios, int(msg), user)
+
+            if resp:
+                bot.send_message(chat_id, "Tudo certo, Hora valida. Mande a data para a qual vai ser a reserva.")
+                r = bot.send_message(chat_id, "Mande a data no seguinte formato dd/mm/yyyy")
+                bot.register_next_step_handler(r, procDate)
+            
+            else:
+                r = bot.send_message(chat_id, "Horario inválido. Tente novamente")
+                bot.register_next_step_handler(r, procHour)
 
     except Exception:
         chat = message.chat.id
@@ -203,52 +207,61 @@ def procHour(message):
 
 
 def procDate(message):
-    data_user = str(message.text)
+    data_user = str(message.text).strip().lower()
     user = str(message.from_user.id)
     chat_id = message.chat.id
 
-    resp, errorCode = botUtil.isDate(data_user, user)
-
-    if resp:
-        r = bot.send_message(chat_id, f"Data valida. A reserva é para você?\n1 - Sim\n2 - Não")
-        bot.register_next_step_handler(r, forYou)
-
+    if "/stop" in data_user:
+            return stop(message)
     else:
-        if errorCode == 6:
-            r = bot.send_message(chat_id, "Data inválida. A data não pode ser posterior a sete dias")
-            bot.register_next_step_handler(r, procDate)
+        resp, errorCode = botUtil.isDate(data_user, user)
 
-        elif errorCode == 7:
-            r = bot.send_message(chat_id,
-                                 "Data inválida. A data não pode ser anterior ao dia de hoje, tente novamente.")
-            bot.register_next_step_handler(r, procDate)
+        if resp:
+            r = bot.send_message(chat_id, f"Data valida. A reserva é para você?\n1 - Sim\n2 - Não")
+            bot.register_next_step_handler(r, forYou)
 
         else:
-            bot.send_message(chat_id, "Formato de data invalido. Envie novamente, siga o exemplo")
-            r = bot.send_message(chat_id, "Ex: 24/10/2021")
-            bot.register_next_step_handler(r, procDate)
+            if errorCode == 6:
+                r = bot.send_message(chat_id, "Data inválida. A data não pode ser posterior a sete dias")
+                bot.register_next_step_handler(r, procDate)
+
+            elif errorCode == 7:
+                r = bot.send_message(chat_id,
+                                    "Data inválida. A data não pode ser anterior ao dia de hoje, tente novamente.")
+                bot.register_next_step_handler(r, procDate)
+
+            else:
+                bot.send_message(chat_id, "Formato de data invalido. Envie novamente, siga o exemplo")
+                r = bot.send_message(chat_id, "Ex: 24/10/2021")
+                bot.register_next_step_handler(r, procDate)
 
 
 def forYou(message):
     try:
         chat_id = message.chat.id
-        msg = int(message.text)
+        msg = str(message.text).strip().lower()
         user = str(message.from_user.id)
-        if msg == 1:
-            botUtil.writeText(user, "para_si", "1")
-            r = bot.send_message(chat_id,
-                                 "Ok, ultima coisa, por questões de segurança o Telegram não me permite  ver seu numero de telelfone, então vou precisar que você mande ele pra mim")
-            bot.register_next_step_handler(r, PhoneNumber)
 
-        elif msg == 2:
-            botUtil.writeText(user, "para_si", "2")
-            r = bot.send_message(chat_id,
-                                 "Ok, ultima coisa, por questões de segurança o Telegram não me permite  ver seu numero de telelfone, então vou precisar qeu você mande ele pra mim")
-            bot.register_next_step_handler(r, PhoneNumber)
-
+        if "/stop" in msg:
+            
+            return stop(message)
+        
         else:
-            r = bot.send_message(chat_id, "Não entendi, essa reserva é para você?\n1 - Sim\n2 - Não")
-            bot.register_next_step_handler(r, forYou)
+            if int(msg) == 1:
+                botUtil.writeText(user, "para_si", "1")
+                r = bot.send_message(chat_id,
+                                    "Ok, ultima coisa, por questões de segurança o Telegram não me permite  ver seu numero de telelfone, então vou precisar que você mande ele pra mim")
+                bot.register_next_step_handler(r, PhoneNumber)
+
+            elif int(msg) == 2:
+                botUtil.writeText(user, "para_si", "2")
+                r = bot.send_message(chat_id,
+                                    "Ok, ultima coisa, por questões de segurança o Telegram não me permite  ver seu numero de telelfone, então vou precisar qeu você mande ele pra mim")
+                bot.register_next_step_handler(r, PhoneNumber)
+
+            else:
+                r = bot.send_message(chat_id, "Não entendi, essa reserva é para você?\n1 - Sim\n2 - Não")
+                bot.register_next_step_handler(r, forYou)
 
     except Exception:
         chat = message.chat.id
@@ -261,15 +274,19 @@ def PhoneNumber(message):
     try:
         chat_id = message.chat.id
         user = str(message.from_user.id)
-        phone = message.text
+        phone = str(message.text).lower().strip()
 
-        resp = botUtil.verificPhoneNumber(phone, user)
+        if "/stop" in phone:
+            return stop(message)
 
-        if resp:
-            callReservation(message)
         else:
-            r = bot.send_message(chat_id, "Numero de telefone invalido, digite APENAS OS NUMEROS do seu telefone")
-            bot.register_next_step_handler(r, PhoneNumber)
+            resp = botUtil.verificPhoneNumber(phone, user)
+
+            if resp:
+                callReservation(message)
+            else:
+                r = bot.send_message(chat_id, "Numero de telefone invalido, digite APENAS OS NUMEROS do seu telefone")
+                bot.register_next_step_handler(r, PhoneNumber)
 
     except Exception:
         chat = message.chat.id
